88f9fd69a8d9ec064b0bd93df9f9f63c51f43d3b
Camleaf <bdaace09@gmail.com>
2026-07-21 20:12:36 -0400

    ball

--- software/ds4-wifi-link/src/main.cpp
+++ software/ds4-wifi-link/src/main.cpp
@@ -1,5 +1,6 @@
 #include "SDL3/SDL_gamepad.h"
 #include "SDL3/SDL_hints.h"
+#include "SDL3/SDL_stdinc.h"
 #include <SDL3/SDL.h>
 #include <iostream>
 #include <state.h>
@@ -16,49 +17,54 @@ using bt = mechmania::buttons;
 
 #define DEADZONE 8000
 #define DEBUG false
-#define PORT     8888
+#define PORT 8888
 
 long map_val(long x, long in_min, long in_max, long out_min, long out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 int main(int argc, char* argv[]) {
-
+    std::cout << "program start" << std::endl;
+    SDL_SetHint(SDL_HINT_NO_SIGNAL_HANDLERS, "1");
     //setup sdl
+
+SDL_SetHint(SDL_HINT_JOYSTICK_HIDAPI, "0");
     if (!SDL_Init(SDL_INIT_GAMEPAD)) {
         std::cerr << "Init failed:" << SDL_GetError() << std::endl;
         return 1;
     }
+    std::cout << "gamepad init completed" << std::endl;
     
 
     SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS,"1");
     bool running = true;
     
     // setup netsocket
-
+    std::cout << "start init netsocket" << std::endl;
     // Create UDP socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+
+        std::cout << "socket creation failed" << std::endl;
+        return 1;
     }
-    
+
+    std::cout << "create addr" << std::endl;
     struct sockaddr_in servaddr;
     memset(&servaddr, 0, sizeof(servaddr));
 
     // Fill server address info
     servaddr.sin_family = AF_INET;              // IPv4
     servaddr.sin_port   = htons(PORT);          // Server port
-    servaddr.sin_addr.s_addr = inet_addr("192.168.x.x"); // Server IP
-     
-    
+    servaddr.sin_addr.s_addr = inet_addr("192.168.4.1"); // Server IP
+    std::cout << "start main" << std::endl;
 
     // run sdl mainloop
     SDL_Event event;
     while (running) {
         // send over network
-        sendto(sockfd,(const char*)cState,sizeof(*cState),MSG_CONFIRM,
-                (const struct sockaddr *)&servaddr, sizeof(servaddr));
+        sendto(sockfd,(const char*)cState,sizeof(*cState),0,
+                  (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
         // handle events
         while (SDL_PollEvent(&event)) {
@@ -87,7 +93,7 @@ int main(int argc, char* argv[]) {
                     }
 
                     std::cout << "gamepad disconnected" << std::endl;
-
+                    break;
 
                 case SDL_EVENT_GAMEPAD_BUTTON_DOWN:
                     std::cout << "Button pressed: " << (int)event.gbutton.button << std::endl;
